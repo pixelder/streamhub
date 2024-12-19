37012714789804b8e68c82b2eca906362e1dc112
Chill guy <160255117+pixelder@users.noreply.github.com>
2024-12-19 19:18:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8443;
 
-app.use(express.static('public')); // Serve files from the 'public' folder
+app.use(express.static('pubic')); // Serve files from the 'public' folder
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
