11455c2dc349eecbf511322ade0336d16482f476
monsieurfeathers <rishikumardeka@outlook.com>
2024-12-28 21:59:10 +0530

    changes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = 8443;
 
 app.use(express.static('public')); // Serve files from the 'public' folder
 
+app.get("/error", (req,res) => {
+  res.sendStatus(500);
+})
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
