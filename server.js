402878aa56beda486abdcad29e4d98bd6b27d6d0
monsieurfeathers <rishikumardeka@outlook.com>
2024-12-29 19:43:39 +0530

    fix for vercel

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
 // Serve static files like CSS, JS
 app.use(express.static(path.join(__dirname, 'public')));
 
