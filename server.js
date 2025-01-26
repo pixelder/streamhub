c614614707d9d30a7468d95ec7dba5b33f3c91b7
monsieurfeathers <rishikumardeka@outlook.com>
2025-01-26 19:58:53 +0530

    added helmet for security

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 const path = require('path');
 
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(helmet());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
