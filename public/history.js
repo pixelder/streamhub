9cd8beaa22f9dea01043b909abf82eea4c9276b1
monsieurfeathers <rishikumardeka@outlook.com>
2025-04-06 11:20:32 +0530

    bug fix

--- public/history.js
+++ public/history.js
@@ -199,12 +199,12 @@ function renderLogItems(data, item, tvData) {
     ? data.backdrop_path
       ? (IMAGE_URL + data.backdrop_path)
       : 'https://placehold.co/440x661/383852/ccc?text=No+Image'
-    : epData.still_path
-      ? (IMAGE_URL + epData.still_path)
+    : epData?.still_path
+      ? (IMAGE_URL + epData?.still_path)
       : (IMAGE_URL + data.backdrop_path);
   const name = (data.title || data.name);
   const info = `S${sno}:E${eno} ` + (epData?.name || '');
-  const rating = truncate(!tvData ? data.vote_average : epData.vote_average, 1);
+  const rating = truncate(!tvData ? data.vote_average : epData?.vote_average, 1);
   //const runTime = !tvData ? data.runtime : epData.runtime;
 
   return `
