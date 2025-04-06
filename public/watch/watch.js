c000ed1badfabfbc6aed00fa81d3df1481314266
monsieurfeathers <rishikumardeka@outlook.com>
2025-04-06 10:37:48 +0530

    added new logging system based on source iframe postmessage added ability to fallback to default logging if no postmessage available added ability to suggest next episode after completion moved functions around

--- public/watch/watch.js
+++ public/watch/watch.js
@@ -5,6 +5,8 @@ const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
 let currentSeason = null;
 let currentEpisode = null;
+let lastSource = 1
+let newSource = 1
 
 async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
   //console.log('loading watchpage')
@@ -13,9 +15,10 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
   currentSeason = season;
   currentEpisode = episode;
 
-  const buildWatchPage = () => {
+  const buildProviderHTML = () => {
     const providers = [
-      { ds: "100", name: "Anime", hiddenOn: 'movie',
+      {
+        ds: "100", name: "Anime", hiddenOn: 'movie',
         settings: [
           {
             type: "format",
@@ -24,19 +27,20 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
               { active: true, value: "DUB" }
             ],
             switches: [
-              { value: "0", active: false},
-              { value: "1", active: true}
+              { value: "0", active: false },
+              { value: "1", active: true }
             ]
           }
         ]
       },
       { ds: "1", name: "Vidlink" },
-      { ds: "2", name: "VidPlay",
+      {
+        ds: "2", name: "VidPlay",
         settings: [
           {
             type: "version",
             label: [
-              { active: true, value:  "v3" },
+              { active: true, value: "v3" },
               { active: false, value: "v2" }
             ],
             switches: [
@@ -47,22 +51,22 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
         ]
       },
       { ds: "3", name: "Vidsrc" },
-      { ds: "4", name: "Whvx", hidden: 'true'},
+      { ds: "4", name: "Whvx", hidden: 'true' },
       { ds: "5", name: "Videasy", hiddenOn: 'tv' },
       { ds: "6", name: "111movies" },
       { ds: "7", name: "Primewire" },
-      { ds: "8", name: "Multiembed" },
-      { ds: "9", name: "AutoEmbed(Multi)" },
+      { ds: "8", name: "VidFast" },
+      { ds: "9", name: "AutoEmbed+" },
       { ds: "10", name: "VidSu" }
     ];
-  
-    const generateSettingsHTML = (settingsArray,ds) => {
+
+    const generateSettingsHTML = (settingsArray, ds) => {
       if (!settingsArray || !Array.isArray(settingsArray)) return '';
       return settingsArray
         .map(({ type, label, switches }) => {
           const switchesHTML = switches.map(sw => {
-              const activeClass = sw.active ? ' active' : '';
-              return `<button class="switch${activeClass}" data-type=${type} data-${type}="${sw.value}"></button>`;
+            const activeClass = sw.active ? ' active' : '';
+            return `<button class="switch${activeClass}" data-type=${type} data-${type}="${sw.value}"></button>`;
           }).join('');
           return `
             <span class="provider-settings" data-source="${ds}">
@@ -75,11 +79,9 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
         })
         .join('');
     };
-  
-    // Generate provider HTML dynamically.
+
     const providersHTML = providers
-      .map(({ ds, name, settings, hiddenOn, hidden}) => {
-        // Conditionally hide providers for TV.
+      .map(({ ds, name, settings, hiddenOn, hidden }) => {
         const styleAttr = (mediaType === `${hiddenOn}` || hidden === 'true') ? "style='display:none'" : "";
         const settingsHTML = generateSettingsHTML(settings, ds);
         return `
@@ -90,13 +92,15 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
         `;
       })
       .join('');
-  
-    return `
+
+    return `${providersHTML}`
+  };
+
+  const templateHTML = `
       <div class="watch-page">
         <div class="player-container">
           <div class="player">
-            <!-- Placeholder until iframe loads -->
-            <div class="loading">Loading player...</div>
+          <div class="loading">Loading player...</div> <!-- Placeholder until iframe loads -->
             <div class="iframe-container"></div>
             <div class="player-toolbar">
               <div class="provider-menu">
@@ -104,7 +108,7 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
                   <i class="fa-solid fa-server"></i>
                 </button>
                 <div class="providers">
-                  ${providersHTML}
+                  ${buildProviderHTML()}
                 </div>
               </div>
               <div class="media-download">
@@ -123,48 +127,47 @@ async function loadWatchPage(mediaType, NAME = null, id, tvData = null) {
         <div class="player-episodes"></div>
       </div>
     `;
-  };
 
-  document.querySelector("#main-content").innerHTML = buildWatchPage()
+  document.querySelector("#main-content").innerHTML = templateHTML
 
-  const {data} = await fetchMetaData(mediaType, id)
+  const { data } = await fetchMetaData(mediaType, id)
   const name = data.title ?? data.name
 
-  const title = `${mediaType === "movie" 
+  const title = `${mediaType === "movie"
     ? `${name} - PixelStream`
     : `S${season}:E${episode} ${name}`} - PixelStream`;
-  const info = `<h2>${name}</h2> ${mediaType === "tv" 
+  const info = `<h2>${name}</h2> ${mediaType === "tv"
     ? `<h4>S${season}:E${episode} ${tvData?.epname}</h4>`
-    : "" }`;
+    : ""}`;
 
   document.querySelector("title").innerText = title;
   document.querySelector(".now-playing").innerHTML = info;
 
-  history.replaceState('','',`/watch/${mediaType}/${id}/${name}${mediaType === 'tv' ? `/${season}/${episode}` : ''}`)
-  
+  history.replaceState('', '', `/watch/${mediaType}/${id}/${name}${mediaType === 'tv' ? `/${season}/${episode}` : ''}`)
+
   if (mediaType == 'tv') {
     await tvContent(data, season, episode, ref = "player");
-    ['click','keydown'].forEach(eventType => {
-      document.removeEventListener(eventType, watchEventListeners)  
+    ['click', 'keydown'].forEach(eventType => {
+      document.removeEventListener(eventType, watchEventListeners)
       document.addEventListener(eventType, watchEventListeners)
     });
   }
 
   let source = getLoggedSource(id) || 1;
-  loadSources(source, mediaType, id, season, episode);
+  setUpPlayer(source, mediaType, id, season, episode);
 
   // Add event listeners to dropdown items
   const sourceSelector = document.querySelectorAll('.provider');
   sourceSelector.forEach(item => {
     item.addEventListener('click', (e) => {
       const provider = item.querySelector('.provider-name')
-      const lastSource = provider.classList.contains('selected');
+      lastSource = provider.classList.contains('selected');
       const settingsChanged = !!e.target.closest('.provider-settings > .switch-buttons')
-      source = Number(provider.getAttribute('data-source'));
-      let settings = getProviderSettings(item) || [ null ]
+      newSource = Number(provider.getAttribute('data-source'));
+      let settings = getProviderSettings(item) || [null]
       if (settingsChanged) settings = setProviderSettings(item);
       if (!lastSource || settingsChanged) {
-        loadSources(source, mediaType, id, currentSeason, currentEpisode, settings);
+        setUpPlayer(newSource, mediaType, id, currentSeason, currentEpisode, settings);
       }
     });
   });
@@ -175,12 +178,12 @@ async function animeEpisodeCounter(metadata, tvData) {
   if (document.querySelectorAll('.episode')[0]?.dataset.id > Number(tvData.eno)) return tvData.eno
 
   let epCount = 0
-  const { sno , eno } = tvData
+  const { sno, eno } = tvData
   if (!metadata.season) return epCount
   metadata?.seasons.forEach(season => {
     //console.log(season.season_number, Number(sno), Number(eno) )
-    if ( season.season_number === 0 || season.season_number > Number(sno) ) return
-    if ( season.season_number === Number(sno)) {
+    if (season.season_number === 0 || season.season_number > Number(sno)) return
+    if (season.season_number === Number(sno)) {
       epCount += Number(eno)
     } else {
       epCount += season.episode_count
@@ -190,19 +193,19 @@ async function animeEpisodeCounter(metadata, tvData) {
   return epCount
 }
 
-async function resolveSource(source,mediaType,id, tvData ) {
+async function resolveSource(source, mediaType, id, tvData) {
   //console.log('resolving source')
   if (source === 100) {
-    const { data, ep } = await animeResolver(mediaType,id, tvData);
-    console.log('AniID: ',data.id);
-    return { ID : data.id, ep };
+    const { data, ep } = await animeResolver(mediaType, id, tvData);
+    console.log('AniID: ', data.id);
+    return { ID: data.id, ep };
   }
-  return { ID : id };
+  return { ID: id };
 }
 
-async function animeResolver(mediaType,id, tvData) {
+async function animeResolver(mediaType, id, tvData) {
   //console.log('fetching ani list id ')
-  const { data: metadata}  = await fetchMetaData(mediaType,id)
+  const { data: metadata } = await fetchMetaData(mediaType, id)
   const ep = await animeEpisodeCounter(metadata, tvData)
   const title = metadata.original_name || metadata.original_title || metadata.name || metadata.title
   //console.log(title)
@@ -218,7 +221,7 @@ async function animeResolver(mediaType,id, tvData) {
       }
     }
   `;
-  
+
   const response = await fetch("https://graphql.anilist.co", {
     method: "POST",
     headers: {
@@ -228,18 +231,12 @@ async function animeResolver(mediaType,id, tvData) {
   });
 
   const data = await response.json();
-  return { data : data.data.Media , ep}
+  return { data: data.data.Media, ep }
 }
 
-async function loadSources(source, mediaType, id, season = null, episode = null, settings = [ null ], ) {
+function setUpPlayer(source, mediaType, id, season = null, episode = null, settings = [null]) {
   loc();
-  //console.log('loading source ifram')
-  const loadIframe = await getSourceIframe(source, mediaType, id, season, episode, settings)
-  // indicicate loading...
-  document.querySelector(".loading").style.display = "flex";
-  document.querySelector(".iframe-container").innerHTML = loadIframe;
 
-  //move this to only change when source does not send error
   const sourceSelector = document.querySelectorAll('.provider-name');
   sourceSelector.forEach(item => {
     if (item.dataset.source === String(source)) {
@@ -248,7 +245,6 @@ async function loadSources(source, mediaType, id, season = null, episode = null,
       item.classList.remove('selected');
     }
   });
-
   const episodeSelector = document.querySelectorAll('.episode');
   episodeSelector.forEach(item => {
     if (item.dataset.episode === String(episode)) {
@@ -258,56 +254,47 @@ async function loadSources(source, mediaType, id, season = null, episode = null,
     }
   });
 
-  const taskId = "logHistory";
-  const duration = 120;
-  cancel(taskId);
+  loadSource(source, mediaType, id, season, episode, settings)
+}
 
-  wait(taskId, duration)
-    .then(() => {
-      logToLocalStorage('history', Number(id), mediaType, season, episode);
-      logToLocalStorage('watching', Number(id), mediaType, season, episode);
-      localStorage.setItem(id, source);
-    })
-    .catch((err) => {
-      if (err.message.includes("Wait canceled")) {
-        console.log("Wait was canceled before completion.");
-      } else {
-        console.error("Error:", err.message);
-      }
-    });
+async function loadSource(source, mediaType, id, season = null, episode = null, settings = [null]) {
+  //console.log('loading source ifram')
+  const loadIframe = await getSourceIframe(source, mediaType, id, season, episode, settings)
+  // indicicate loading...
+  document.querySelector(".loading").style.display = "flex";
+  document.querySelector(".iframe-container").innerHTML = loadIframe;
 }
 
 function setProviderSettings(item) {
-  item.querySelectorAll('.switch').forEach(btn =>{
+  item.querySelectorAll('.switch').forEach(btn => {
     btn.classList.toggle('active');
   })
   const label = item.querySelector(".type")
   const button = item.querySelector(".switch-buttons")
   const status = label.dataset.status
   label.innerText = `${button.dataset[status === 'true' ? 'inactive' : 'active']}`
-  label.setAttribute('data-status', `${status === 'true' ? '' : 'true'}` )
+  label.setAttribute('data-status', `${status === 'true' ? '' : 'true'}`)
   return getProviderSettings(item)
 }
 
 function getProviderSettings(item) {
   const btn = item.querySelector('.switch.active')
   if (!btn) return
-  const { type }  = btn.dataset
-  const settings = {[type]: btn.dataset[type]}
+  const { type } = btn.dataset
+  const settings = { [type]: btn.dataset[type] }
   //console.log(settings[type])
   if (!settings) return
   return settings
 }
 
-async function getSourceIframe(source, mediaType, id, season = null, episode = null, settings = [ null ]) {
+async function getSourceIframe(source, mediaType, id, season = null, episode = null, settings = [null]) {
   let src = "";
-  //console.log('generating source iframe')
-  
-  const tvData = { sno: season, eno: episode}
-  const { ID : ID , ep } = await resolveSource(source,mediaType,id,tvData)
+
+  const tvData = { sno: season, eno: episode }
+  const { ID: ID, ep } = await resolveSource(source, mediaType, id, tvData)
   const urlPath = `${mediaType}/${ID}${season && episode ? `/${season}/${episode}` : ''}`
 
-  const { version, format } = settings ? settings : [ null ];
+  const { version, format } = settings ? settings : [null];
 
   switch (source) {
     case 100:
@@ -334,15 +321,18 @@ async function getSourceIframe(source, mediaType, id, season = null, episode = n
     case 7:
       src = `https://primewire.tf/embed/${mediaType}?tmdb=${ID}${season && episode ? `&season=${season}&episode=${episode}` : ''}`;
       break;
+    // case 8:
+    //   src = `https://multiembed.mov/?video_id=${ID}&tmdb=1${season && episode ? `&s=${season}&e=${episode}` : ''}`;
+    //   break;
     case 8:
-      src = `https://multiembed.mov/?video_id=${ID}&tmdb=1${season && episode ? `&s=${season}&e=${episode}` : ''}`;
+      src = `https://vidfast.pro/${urlPath}?poster=false`
       break;
     case 9:
       src = `https://hin.autoembed.cc/${urlPath}`;
       break;
     case 10:
       src = `https://vidsrc.su/embed/${urlPath}`;
-      break;  
+      break;
     default:
       console.error("Invalid source selected");
       return;
@@ -353,6 +343,8 @@ async function getSourceIframe(source, mediaType, id, season = null, episode = n
       <i class="fa-solid fa-compress"></i>
     </button>
     <iframe
+      data-id="${id}"
+      data-media-type="${mediaType}"
       src="${src}"
       frameborder="0"
       scrolling="no"
@@ -376,5 +368,85 @@ function showIframe(iframe) {
   //console.log('source iframe is loaded')
   iframe.style.display = "block";
   document.querySelector(".loading").style.display = "none";
+  const { id, mediaType } = iframe.dataset
+  setupLogging(Number(id), mediaType)
   cropToFit()
+}
+
+function setupLogging(id, mediaType) {
+
+  let defaultLogWait;
+
+  const defLoggingSys = (duration, id, mediaType) => {
+    const taskId = "logHistory";
+    cancel(taskId);
+
+    wait(taskId, duration)
+      .then(() => {
+        logToLocalStorage('watching', Number(id), mediaType, currentSeason, currentEpisode);
+        const data = getLogData('lastStored')
+        console.log(data)
+        if (logExists('lastStored', id, mediaType, currentSeason, currentEpisode)) {
+          removeFromLocalStorage('history', Number(id), mediaType, currentSeason, currentEpisode, data[0].index)
+        }
+        logToLocalStorage('history', Number(id), mediaType, currentSeason, currentEpisode);
+        localStorage.setItem(id, newSource);
+      })
+      .catch((err) => {
+        if (err.message.includes("Wait canceled")) {
+          console.log("Wait was canceled before completion.");
+        } else {
+          console.error("Error:", err.message);
+        }
+      });
+  }
+
+  defaultLogWait = setTimeout(() => {
+    defLoggingSys(20, id, mediaType)
+  }, 10000)
+
+  let logFlag = false;
+  setInterval(function () {
+    logFlag = true;
+  }, 5000);
+
+  const postMsgLogging = (e) => {
+    if (e) clearTimeout(defaultLogWait)
+    if (!logFlag || e.data.type === 'MEDIA_DATA') return
+    console.log(e.origin)
+    logFlag = false
+
+    if (e.data.data.event !== 'timeupdate') return
+    const progress = truncate(100 * (e.data.data.currentTime / e.data.data.duration), 2)
+    if (5 < progress && progress < 85) {
+      const data = getLogData('lastStored')
+      if (logExists('lastStored', id, mediaType, currentSeason, currentEpisode)) {
+        removeFromLocalStorage('history', Number(id), mediaType, currentSeason, currentEpisode, data[0].index)
+      }
+
+      ['watching', 'history'].forEach(logType => {
+        logToLocalStorage(logType, Number(id), mediaType, currentSeason, currentEpisode, progress);
+      })
+
+      localStorage.setItem(id, newSource);
+    }
+
+    if (90 < progress) {
+
+      logToLocalStorage('history', Number(id), mediaType, currentSeason, currentEpisode, 100);
+
+      if (mediaType === 'movie') {
+        removeFromLocalStorage('watching', Number(id), mediaType)
+        return
+      }
+      getNextEpisode(id, currentSeason, currentEpisode).then((ep) => {
+        if (!ep) return
+        //log the next episode
+        logToLocalStorage('watching', Number(id), 'tv', ep.season_number, ep.episode_number)
+      })
+    }
+  }
+
+  window.removeEventListener('message', postMsgLogging)
+  window.addEventListener('message', postMsgLogging);
 }
