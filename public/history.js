3eafeefd3bc720d255d12fb3e3345fe6d36cff11
monsieurfeathers <rishikumardeka@outlook.com>
2025-04-28 12:18:06 +0530

    small changes

--- public/history.js
+++ public/history.js
@@ -196,9 +196,7 @@ function renderLogItems(data, item, tvData) {
   const [sno, eno] = tvData ? [item.data.sno, item.data.eno] : ['', ''];
   const epData = tvData ? tvData?.episodes?.find(ep => ep.episode_number === Number(eno)) : '';
   const progress = Number(item.progress) || 0;
-  //console.log() 
-  // add fake data to through error then create function to prompt to delete that data
-  // should be done on above function
+  
   const image = !tvData
     ? data.backdrop_path
       ? (IMAGE_342 + data.backdrop_path)
