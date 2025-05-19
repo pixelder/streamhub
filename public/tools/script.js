a85145468e715930e4b612ed5fb83fa639c419d3
monsieurfeathers <rishikumardeka@outlook.com>
2025-05-19 23:43:41 +0530

    bug fix

--- public/tools/script.js
+++ public/tools/script.js
@@ -126,7 +126,7 @@ let controller
 let parsedCount = 0;
 let parsable = 0;
 let error = false;
-const SERVER = "https://alph=a-scraper.onrender.com";
+const SERVER = "https://alpha-scraper.onrender.com";
 // const SERVER = "http://192.168.29.122:3000"
 
 async function  fetchAndRender({payload, output, resultsDiv}) {
