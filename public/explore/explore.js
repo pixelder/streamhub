6e7168bb4f5a3af2f59c39f9083189cf94180878
monsieurfeathers <rishikumardeka@outlook.com>
2025-06-22 12:49:18 +0530

    small changes

--- public/explore/explore.js
+++ public/explore/explore.js
@@ -687,20 +687,20 @@ async function handleChipClick(genreId, type, chipList) {
 	genreId = parseInt(genreId);
 	const chip = document.querySelector(`.chip[data-id="${genreId}"]`)
 	const parent = chip.parentElement;
-	const unTouched = () => document.querySelectorAll('.chip:not(.excluded, .selected)')
-	const order = ({second, remove} = {}) => {
+	// const unTouched = () => document.querySelectorAll('.chip:not(.excluded, .selected)')
+	const order = ({exclude, remove} = {}) => {
 		const i = chipList.findIndex(el => el === Number(chip.dataset.id))
 		let index = 0
 		Array.from(document.querySelectorAll('.chip')).forEach(item => {
 			if (!remove && item.classList.contains('selected')) {
-				if (second) {
+				if (exclude) {
 					index++
 					return
 				}
 				const j = chipList.findIndex(el => el === Number(item.dataset.id))
 				if (i > j) index++
 			}
-			if (!remove && second && item.classList.contains('excluded')) {
+			if (!remove && exclude && item.classList.contains('excluded')) {
 				const j = chipList.findIndex(el => el === Number(item.dataset.id))
 				if (i > j) index++
 			}
@@ -716,9 +716,9 @@ async function handleChipClick(genreId, type, chipList) {
 		return Array.from(document.querySelectorAll('.chip'))[index]
 	}
 
-	const ADD = (second = null) => {
-		const position = second
-			? order({second: true})//unTouched[0]
+	const ADD = ({exclude} = {}) => {
+		const position = exclude
+			? order({exclude: true})
 			: order()
 		parent.insertBefore(chip, position)
 	}
@@ -726,13 +726,6 @@ async function handleChipClick(genreId, type, chipList) {
 	const REMOVE = () => {
 	  const position = order({remove: true})
 		parent.insertBefore(chip, position)
-//		const unTouchedList = () => Array.from(unTouched())
-//		unTouchedList().push(chip)
-//		unTouchedList().forEach(chip => {
-//			const index = chipList.findIndex(el => el === Number(chip.dataset.id))
-//			const position = unTouched()[index - (chipList.length - unTouched().length)]
-//			parent.insertBefore(chip, position)
-//		})
 	}
 
 	if (type === 'single') {
@@ -755,7 +748,7 @@ async function handleChipClick(genreId, type, chipList) {
 		if (!excludedGenres.includes(genreId)) {
 			excludedGenres.push(genreId);
 			selectedGenres = selectedGenres.filter(id => id !== genreId); // Ensure it's not in selectedGenres
-			ADD(true)
+			ADD({exclude: true})
 		} else {
 			excludedGenres = excludedGenres.filter(id => id !== genreId);
 			REMOVE()
