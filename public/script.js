d75a55259beda4e89fc3909e9b453013e8df1b55
monsieurfeathers <rishikumardeka@outlook.com>
2026-05-03 09:42:33 +0530

    removed name from watch url to avoid link breakage

--- public/script.js
+++ public/script.js
@@ -470,9 +470,9 @@ function carouselEvents(carousel) {
         ])
       );
 
-      const { id, name, type } = sanitizedData;
+      const { id, type } = sanitizedData;
 
-      if (play) window.location.href = `/watch/${type}/${id}/${name}${type === 'tv' ? `/1/1` : ''}`;
+      if (play) window.location.href = `/watch/${type}/${id}/${type === 'tv' ? `/1/1` : ''}`;
       if (detail) openModal({ id, mediaType: type });
 
       e.stopPropagation();
